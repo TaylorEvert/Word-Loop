e1ddcff424a736a307ef2c1fdbd6dd8ada97a343
TaylorEvert <tevert78@gmail.com>
2023-03-01 15:05:44 -0600

    implement working api

--- app/src/main/java/com/firebaseauthdemo/guessAiWordActivity.kt
+++ app/src/main/java/com/firebaseauthdemo/guessAiWordActivity.kt
@@ -1,11 +1,57 @@
 package com.firebaseauthdemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.firebaseauthdemo.randomword.RandomWordData
+import com.firebaseauthdemo.randomword.RandomWordInterface
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+// API URL
+const val BASE_URL = "https://api.api-ninjas.com/v1/"
+// TAG
+const val TAG = "guessAiWordActivity"
 class guessAiWordActivity : AppCompatActivity() {
+
+    // Declare activity view variables
+    lateinit var view_word: Button
+    lateinit var view_strike: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_guess_ai_word)
+
+        getRandomWord()
+
+    }
+
+    // Uses retrofit to create api instance, grabs a random word
+    private fun getRandomWord() {
+
+        GlobalScope.launch { // Create retrofit simpleton
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val wordApi = retrofit.create(RandomWordInterface::class.java)
+            val response = wordApi.getWord()
+
+            if (response.isSuccessful && response.body() != null) {
+                print(response.body()!!.word)
+                Log.e(TAG, "woohoo!", )
+                view_word = findViewById(R.id.btn_random_word)
+                view_word.text = response.body()!!.word
+            } else {
+                Log.e(TAG, "failure")
+            }
+
+        }
     }
 }
--- app/src/main/java/com/firebaseauthdemo/randomword/RandomWordInterface.kt
+++ app/src/main/java/com/firebaseauthdemo/randomword/RandomWordInterface.kt
@@ -1,6 +1,7 @@
 package com.firebaseauthdemo.randomword
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Headers
 
@@ -8,5 +9,5 @@ interface RandomWordInterface {
 
     @GET("randomword")
     @Headers("X-Api-Key: tySaRZBIbSKCkfr3xiesyLoAQvdFCH0ivKQrhC7Z")
-    fun getWord(): Call<List<RandomWordData>>
+    suspend fun getWord(): Response<RandomWordData>
 }
